fc6a237d84d3d63c209a74f0edc3a91b34448eec
GoldenFox <simon270906@gmail.com>
2023-11-14 21:06:10 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/ametask/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ametask
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/homepage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      color: Color(0xFF2D2E2F),
+    );
+  }
+}
