5accb71606b36d74a0fe1df54757f353c9732f4b
GoldenFox <simon270906@gmail.com>
2023-11-15 17:05:10 +0100

    navigation bar created

--- lib/home/homepage.dart
+++ lib/home/homepage.dart
@@ -5,9 +5,75 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF2D2E2F),
+      bottomNavigationBar: NavigationBar(),
+    );
+  }
+
+  Widget NavigationBar() {
     return Container(
-      height: 200,
-      color: Color(0xFF2D2E2F),
+      child: Container(
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Color(0xFF1D1D1D),
+            selectedItemColor: const Color(0xFFA562E4),
+            selectedFontSize: 12,
+            unselectedFontSize: 12,
+            unselectedItemColor: Colors.grey.withOpacity(0.7),
+            type: BottomNavigationBarType.fixed,
+            items: [
+              const BottomNavigationBarItem(
+                label: 'home',
+                icon: Icon(
+                  Icons.home_rounded,
+                  size: 50,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: "example",
+                icon: Container(
+                  margin: const EdgeInsets.all(5),
+                  padding: const EdgeInsets.all(5),
+                  child: const Icon(
+                    Icons.more_horiz_outlined,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: "example",
+                icon: Container(
+                  margin: const EdgeInsets.all(5),
+                  padding: const EdgeInsets.all(5),
+                  child: const Icon(
+                    Icons.more_horiz_rounded,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: "Settings",
+                icon: Container(
+                  margin: const EdgeInsets.all(5),
+                  padding: const EdgeInsets.all(5),
+                  child: const Icon(
+                    Icons.settings,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
