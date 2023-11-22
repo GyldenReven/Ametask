9cfa66858dc45b2fc27dd531d0f256aea7b56462
GoldenFox <simon270906@gmail.com>
2023-11-22 14:56:04 +0100

    tuto fin CRUD

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -36,13 +36,67 @@ CREATE TABLE $tableTasklists (
   ${TasklistFields.id} $idType,
   ${TasklistFields.idFolder} $integerType,
   ${TasklistFields.name} $textType,
-  ${TasklistFields.colors} $textType,
+  ${TasklistFields.color} $textType,
   ${TasklistFields.createDate} $textType,
   ${TasklistFields.lastModifDate} $textType,
-  ${TasklistFields.lastModifHour} $textType,
-
 )
 ''');
+  }
+
+  Future<Tasklist> createTasklist(Tasklist tasklist) async {
+    final db = await instance.database;
+
+    // autre solution (voir la vidéo à 13 min)
+    final id = await db.insert(tableTasklists, tasklist.toJson());
+    return tasklist.copy(id : id);
+  }
+
+  Future<Tasklist> readTasklist(int id) async {
+    final db = await instance.database;
+
+    final maps = await db.query(
+      tableTasklists, 
+      columns: TasklistFields.values,
+      where: '${TasklistFields.id} = ?',
+      whereArgs: [id],   // replace the '?' (prevent sql injection attacks)
+    );
+    if (maps.isNotEmpty) {
+      return Tasklist.fromJson(maps.first);
+    } else {
+      throw Exception('ID $id not found');
+    }
+  }
+
+  Future<List<Tasklist>> readAllTasklists() async {
+    final db = await instance.database;
+
+    final orderBy = '${TasklistFields.createDate} ASC';
+
+    // autre méthode avec le rawQuery
+    final result = await db.query(tableTasklists, orderBy: orderBy);
+
+    return result.map((json) => Tasklist.fromJson(json)).toList();
+  }
+
+  Future updateTasklist(Tasklist tasklist) async {
+    final db = await instance.database;
+
+    return db.update(
+      tableTasklists,
+      tasklist.toJson(),
+      where: '${TasklistFields.id} = ?',
+      whereArgs: [tasklist.id],
+    ); 
+  }
+
+  Future<int> deleteTasklist(int id) async {
+    final db = await instance.database;
+
+    return await db.delete(
+      tableTasklists,
+      where: '${TasklistFields.id} = ?',
+      whereArgs: [id],
+    );
   }
 
   Future close() async {
--- lib/models/tasklists_model.dart
+++ lib/models/tasklists_model.dart
@@ -1,31 +1,69 @@
 final String tableTasklists = 'tasklists';
 
 class TasklistFields {
+  static final List<String> values = [
+    /// add all fields
+    id, idFolder, name, color, createDate, lastModifDate,
+  ];
+
   static final String id = '_id';
   static final String idFolder = 'idFolder';
   static final String name = 'name';
-  static final String colors = 'colors';
+  static final String color = 'color';
   static final String createDate = 'createDate';
   static final String lastModifDate = 'lastModifDate';
-  static final String lastModifHour = 'lastModifHour';
 }
 
-class TasklistModel {
+class Tasklist {
   final int? id;
   final int idFolder;
   final String name;
-  final String colors;
-  final String createDate;
-  final String lastModifDate;
-  final String lastModifHour;
+  final String color;
+  final DateTime createDate;
+  final DateTime lastModifDate;
 
-  const TasklistModel({
+  const Tasklist({
     this.id,
     required this.idFolder,
     required this.name,
-    required this.colors,
+    required this.color,
     required this.createDate,
     required this.lastModifDate,
-    required this.lastModifHour,
   });
+
+  Map<String, Object?> toJson() => {
+        TasklistFields.id: id,
+        TasklistFields.idFolder: idFolder,
+        TasklistFields.name: name,
+        TasklistFields.color: color,
+        TasklistFields.createDate: createDate.toIso8601String(),
+        TasklistFields.lastModifDate: lastModifDate.toIso8601String(),
+      };
+
+  static Tasklist fromJson(Map<String, Object?> json) => Tasklist(
+        id: json[TasklistFields.id] as int?,
+        idFolder: json[TasklistFields.idFolder] as int,
+        name: json[TasklistFields.name] as String, 
+        color: json[TasklistFields.color] as String,
+        createDate: DateTime.parse(json[TasklistFields.createDate] as String),
+        lastModifDate: DateTime.parse(json[TasklistFields.lastModifDate] as String),
+      );
+
+  Tasklist copy({
+    int? id,
+    int? idFolder,
+    String? name,
+    String? color,
+    DateTime? createDate,
+    DateTime? lastModifDate,
+    String? lastModifHour,
+  }) =>
+      Tasklist(
+          id: id ?? this.id,
+          idFolder: idFolder ?? this.idFolder,
+          name: name ?? this.name,
+          color: color ?? this.color,
+          createDate: createDate ?? this.createDate,
+          lastModifDate: lastModifDate ?? this.lastModifDate,
+      );
 }
