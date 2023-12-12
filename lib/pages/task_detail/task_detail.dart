9dc29aa4f5d9aafd023cb1a87e871746e29800c6
GoldenFox <simon270906@gmail.com>
2023-12-12 19:12:54 +0100

    design des tasks

--- lib/pages/task_detail/task_detail.dart
+++ lib/pages/task_detail/task_detail.dart
@@ -49,8 +49,8 @@ class _TaskDetailState extends State<TaskDetail> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 750,
-      padding: const EdgeInsets.all(15),
+      height: 700,
+      padding: const EdgeInsets.all(10),
       decoration: const BoxDecoration(
           color: Color(0xFF2C3158),
           borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
@@ -59,12 +59,18 @@ class _TaskDetailState extends State<TaskDetail> {
           : Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [backButton(context), deleteButton(context)],
+                ),
                 Container(
                   padding: const EdgeInsets.symmetric(vertical: 5),
                   child: Text(
                     "Title :",
-                    style:
-                        GoogleFonts.poppins(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w500),
+                    style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
                   ),
                 ),
                 TextFormField(
@@ -107,8 +113,10 @@ class _TaskDetailState extends State<TaskDetail> {
                   padding: const EdgeInsets.symmetric(vertical: 5),
                   child: Text(
                     "Description :",
-                    style:
-                        GoogleFonts.poppins(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w500),
+                    style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
                   ),
                 ),
                 TextFormField(
@@ -145,15 +153,116 @@ class _TaskDetailState extends State<TaskDetail> {
                     await AmetaskDatabase.instance.updateTask(task);
                   },
                 ),
-                deleteButton(context),
+                const Divider(
+                  color: Color(0x00000000),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      child: Text(
+                        "priority :",
+                        style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 70,
+                      child: TextFormField(
+                        maxLines: 1,
+                        keyboardType: TextInputType.number,
+                        initialValue: (task.position + 1).toString(),
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                            fontSize: 18,
+                            color: Color(0xFFFEFEFE),
+                            fontWeight: FontWeight.w500),
+                        decoration: InputDecoration(
+                          fillColor: const Color(0xFF222645),
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(
+                              width: 0,
+                              style: BorderStyle.none,
+                            ),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          hintStyle: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white.withOpacity(0.3),
+                          ),
+                        ),
+                        //controller: TextEditingController(),
+                        onFieldSubmitted: (String value) async {
+                          List<Task> friendsTasks = await AmetaskDatabase
+                              .instance
+                              .readAllTasksFor(task.idTasklist);
+                          int newPos;
+                          int oldPos = task.position;
 
-                Checkbox(
+                          if (value == "") {
+                            newPos = friendsTasks.length - 1;
+                          } else {
+                            newPos = int.parse(value) - 1;
+                            if (newPos < 0) {
+                              newPos = 0;
+                            }
+                            if (newPos > friendsTasks.length) {
+                              newPos = friendsTasks.length - 1;
+                            }
+                          }
+
+                          if (newPos > oldPos) {
+                            for (var friend in friendsTasks) {
+                              if (friend.position > oldPos &&
+                                  friend.position < oldPos) {
+                                await AmetaskDatabase.instance.updateTask(
+                                    friend.copy(position: friend.position - 1));
+                              }
+                            }
+                          } else if (newPos < oldPos) {
+                            for (var friend in friendsTasks) {
+                              if (friend.position < oldPos &&
+                                  friend.position > oldPos) {
+                                await AmetaskDatabase.instance.updateTask(
+                                    friend.copy(position: friend.position + 1));
+                              }
+                            }
+                          }
+
+                          task = task.copy(position: newPos);
+
+                          await AmetaskDatabase.instance.updateTask(task);
+                        },
+                      ),
+                    ),
+                    const VerticalDivider(
+                      width: 20,
+                      thickness: 1,
+                      indent: 20,
+                      endIndent: 15,
+                      color: Colors.grey,
+                    ),
+                    Text(
+                      "Finished ?",
+                      style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Checkbox(
                         activeColor: const Color(0xFF9B71CF),
                         value: task.finished,
                         onChanged: (bool? value) async {
                           task = task.copy(finished: value);
-                          await AmetaskDatabase.instance
-                              .updateTask(task);
+                          await AmetaskDatabase.instance.updateTask(task);
                           refreshTask();
                         },
                         side: BorderSide.none,
@@ -166,6 +275,8 @@ class _TaskDetailState extends State<TaskDetail> {
                           }
                           return const Color(0xFF3F4678);
                         })),
+                  ],
+                ),
               ],
             ),
     );
@@ -177,6 +288,14 @@ class _TaskDetailState extends State<TaskDetail> {
         onPressed: () async {
           await AmetaskDatabase.instance.deleteTask(widget.taskId);
 
+          Navigator.of(context).pop();
+        },
+      );
+
+  Widget backButton(BuildContext context) => IconButton(
+        icon: const Icon(FeatherIcons.arrowLeft),
+        color: const Color(0xFFFEFEFE),
+        onPressed: () {
           Navigator.of(context).pop();
         },
       );
